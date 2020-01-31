ae785e4fa941b90b1cefb1d370b4e9b2a6a5296a
Harrissou Sant-anna <delazj@gmail.com>
2020-01-31 13:05:09 +0100

    Add disqus for page comment to requirements
    and do some configuration

--- source/conf.py
+++ source/conf.py
@@ -52,6 +52,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.extlinks',
     'sphinx.ext.doctest',
+    'sphinxcontrib.disqus',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
